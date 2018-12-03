28dd6ea8bd7dd3222a7e48a86faef8b6e25eee42
Taras Biletsky <taras.biletsky24@gmail.com>
2018-12-03 14:48:02 +0200

    Closes #95 Add DTO classes for group chat

---
+++ LearnWithMentorDTO/GroupChatMessageDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnWithMentorDTO
+{
+    class GroupChatMessageDTO
+    {
+        public GroupChatMessageDTO(int messageId, int senderId, string text)
+        {
+            MessageId = messageId;
+            SenderId = senderId;
+            Text = text;
+        }
+
+        public int MessageId;
+        public int SenderId;
+        public string Text;
+    }
+}
