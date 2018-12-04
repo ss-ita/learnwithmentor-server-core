1e3042837814e2926989079f68463d849143e269
Mykhailo Stepanov <mstepantc@training.local>
2018-12-04 13:09:59 +0200

    Closes #98 - Added notification hub and required services

---
+++ LearnWithMentor/Controllers/NotificationController.cs
@@ -0,0 +1,12 @@
+﻿using LearnWithMentor.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LearnWithMentor.Controllers
+{
+    [AllowAnonymous]
+    public class NotificationController : Hub<IHubClient>
+    {
+
+    }
+}
