ef2891d508aecee13b20b3216d46f9bde4e55f15
Stanislav Zhurskyi <szhurstc@training.local>
2018-12-07 23:36:49 +0200

    Closes #120 - Add NotificationService

--- LearnWithMentor.BLL/Interfaces/INotificationService.cs
+++ LearnWithMentor.BLL/Interfaces/INotificationService.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using LearnWithMentor.DAL.Entities;
 using LearnWithMentorDTO;
 
 namespace LearnWithMentorBLL.Interfaces
 {
     public interface INotificationService:IDisposableService
-    { 
-        Task<NotificationDTO> GetCommentAsync(int commentId);
-        
+    {
+        Task<bool> AddNotificationAsync(string text, string type, int userId);
+        void MarkNotificationsAsRead(IEnumerable<Notification> notifications);
+        Task<IEnumerable<Notification>> GetNotificationsByUserIdAsync(int userId, int amount);
     }
 }
--- LearnWithMentor.BLL/Services/NotificationsService.cs
+++ LearnWithMentor.BLL/Services/NotificationsService.cs
@@ -7,11 +7,37 @@ using System.Threading.Tasks;
 
 namespace LearnWithMentorBLL.Services
 {
-    class NotificationsService : BaseService
+    class NotificationsService : BaseService,INotificationService
     {
         public NotificationsService(IUnitOfWork db) : base(db)
         {
 
+        }
+
+        public async Task<bool> AddNotificationAsync(string text, string status, int userId)
+        {
+            if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(status))
+                return false;
+            var notificationNew = new Notification
+            {
+                Text = text,
+                Status = status,
+                UserId = userId
+            };
+            await db.Notification.AddAsync(notificationNew);
+            db.Save();
+            return true;
+        }
+
+        public void MarkNotificationsAsRead(IEnumerable<Notification> notifications)
+        {
+            db.Notification.MarkNotificationAsReadAsync(notifications);
+        }
+
+        public async Task<IEnumerable<Notification>> GetNotificationsByUserIdAsync(int userId, int amount)
+        {
+            var notificationsByUser = await db.Notification.GetNotificationsAsync(userId, amount);
+            return notificationsByUser;
         }
     }
 }
